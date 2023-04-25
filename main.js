b85e02a93a92a4d9a0b6d1e3d496e3dd49e594ed
21hardik12 <21hardik12@gmail.com>
2023-04-26 03:14:12 +0530

    Write function to play a single round

--- main.js
+++ main.js
@@ -3,3 +3,30 @@ function getComputerChoice() {
     let choice = choices[Math.floor(Math.random() * 3)];
     return choice;
 }
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    let result;
+    if (playerSelection === "rock" && computerSelection === "scissors") {
+        //console.log("You Win! Rock beats Scissors");
+        result = "You Win! Rock beats Scissors";
+    } else if (playerSelection === "rock" && computerSelection === "paper") {
+        result = "You Lose! Paper beats Rock";
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+        result = "You Win! Paper beats Rock";
+    } else if (playerSelection === "paper" && computerSelection === "scissors") {
+        result = "You Lose! Scissors beats Paper";
+    } else if (playerSelection === "scissors" && computerSelection === "rock") {
+        result = "You Lose! Rock beats Scissors";
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        result = "You Win! Scissors beats Paper";
+    } else {
+        result = `It's a Draw! You both selected ${playerSelection}`;
+    }
+
+    return result;
+}
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
